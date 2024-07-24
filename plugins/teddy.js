40b3ffe0ecfd2a88af1cbcd2aea792cd97ad4549
⚔ DEXTER ⚔ <143838400+DEXTER-BOTS@users.noreply.github.com>
2024-07-25 02:15:50 +0530

    Update teddy.js

--- plugins/teddy.js
+++ plugins/teddy.js
@@ -1,41 +1,19 @@
-const { smd, sleep } = require('../lib');
-const teddyM = { "smd": "asta" };
+const { tlang,cmd } = require('../lib')
+const Config = require('../config')
+const axios = require('axios')
+// Put here your url with modified words and urls
+const url = 'https://gist.github.com/vajirabot1/fba3244ca770725d9c4d61e321c695d2/raw'
 
-smd({
-    cmdname: "teddy",
-    type: "fun",
-    info: "cute teddy",
-    on: "text",
-    filename: __filename,
-}, async (citel, match, { smd }) => {
-    let isteddy = smd === "teddy" ? true : citel.isPublic && match.toLowerCase().includes("teddy") ? true : "";
-    if (isteddy && !teddyM[citel.id]) {
-        teddyM[citel.id] = true;
-        let teddy = ['❤', '💕', '😻', '🧡', '💛', '💚', '💙', '💜', '🖤', '❣', '💞', '💓', '💗', '💖', '💘', '💝', '💟', '♥', '💌', '🙂', '🤗', '😌', '😉', '🤗', '😊', '🎊', '🎉', '🎁', '🎈'];
-        const { key } = await citel.reply(`(\\_/)\n( •.•)\n/>🤍`);
-        for (let i = 0; i < teddy.length; i++) {
-            await sleep(500);
-            await citel.reply(`(\\_/)\n( •.•)\n/>${teddy[i]}`, { edit: key });
-        }
-    }
-});
-
-// Function to send a voice message
-async function sendVoiceMessage(citel) {
-    const mediaUrl = 'https://github.com/purnapurna2007/Audio-mp3/raw/main/Audio/hmmm.mp3';
-
-    // Assuming citel.sendMessage is the method to send media messages
-    await citel.sendMessage(citel.from, { audio: { url: mediaUrl }, mimetype: 'audio/mp4' });
-
-    console.log('Voice message sent successfully');
+/**
+ cmd({
+            pattern: "bgmnsew",
+            category: "owner",
+            use: '',
+        },
+**/ 
+cmd({ on: "text" }, async (Void,citel,text,{isCreator})=> {
+  let { data } = await axios.get(url)
+  for (vr in data){
+ if((new RegExp(`\\b${vr}\\b`,'gi')).test(citel.text)) return Void.sendMessage(citel.chat,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:citel})   
 }
-
-// Replace this function call with your actual message receiving logic
-async function onMessageReceived(citel) {
-    if (citel.body.toLowerCase() === 'hi') {
-        await sendVoiceMessage(citel);
-    }
-}
-
-// Simulate message received event
-onMessageReceived({ body: 'hi', from: 'recipient-phone-number', sendMessage: (to, message) => console.log(`Message sent to ${to}:`, message) });
+})
