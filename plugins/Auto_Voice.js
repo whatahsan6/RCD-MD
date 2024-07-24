73b3d1356839d8f056adfa6223480e06bae9b40a
⚔ DEXTER ⚔ <143838400+DEXTER-BOTS@users.noreply.github.com>
2024-07-25 03:33:28 +0530

    Update Auto_Voice.js

--- plugins/Auto_Voice.js
+++ plugins/Auto_Voice.js
@@ -1,11 +1,9 @@
 const { tlang, cmd } = require('../lib');
-const Config = require('../config');
 const axios = require('axios');
 
 // URL with modified words and their corresponding audio URLs
-const url = 'https://gist.github.com/purnapurna2007/c78c88f763b70239ce3fb4ef31958d1a';
+const url = 'https://gist.githubusercontent.com/purnapurna2007/c78c88f763b70239ce3fb4ef31958d1a/raw/audio_urls.json';
 
-// Register the command
 cmd({
     pattern: "bgmnsew",
     category: "owner",
@@ -14,11 +12,16 @@ cmd({
     try {
         // Fetch the data from the URL
         let { data } = await axios.get(url);
+        console.log('Fetched data:', data);
 
         // Loop through the data to check if the text contains any of the words
         for (let vr in data) {
+            console.log('Checking word:', vr);
+
             // Use regular expression to check for the word in the text
             if ((new RegExp(`\\b${vr}\\b`, 'gi')).test(citel.text)) {
+                console.log('Match found for word:', vr);
+
                 // Send audio message if a match is found
                 return Void.sendMessage(citel.chat, {
                     audio: { url: data[vr] },
